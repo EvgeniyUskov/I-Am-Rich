1a69b231c719b324f97fba68e2c94a8298a88397
Evgeniy Uskov <evgeniy-uskov@Evgeniys-MacBook-Pro.local>
2019-07-03 10:17:39 +0700

    create a fully functinal I Am Rich App

---
+++ I Am Rich/I Am Rich/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  I Am Rich
+//
+//  Created by Evgeniy Uskov on 24/05/2019.
+//  Copyright © 2019 Evgeniy Uskov. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
